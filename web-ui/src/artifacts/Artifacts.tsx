b141fdfdcafb2c44ecaed29a43e72a4d3a60c0e4
amandajzhu <amandazhu0905@gmail.com>
2025-12-18 13:25:50 -0500

    homepage setup

---
+++ web-ui/src/artifacts/Artifacts.tsx
@@ -0,0 +1,16 @@
+import "./Artifacts.css"
+
+function Artifacts() {
+    return (
+        <div className="artifacts">
+            <button className="artifact1">About this Website</button>
+            <button className="artifact2">30-60-90 Day Plan</button>
+            <button className="artifact3">Mentorship Program</button>
+            <button className="artifact4">Wattpad.com</button>
+            <button className="artifact5">Account Settings Page</button>
+            <button className="artifact6">Updated Resume</button>
+        </div>
+    )
+}
+
+export default Artifacts;
